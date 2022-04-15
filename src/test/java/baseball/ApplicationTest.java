f5a1a6f3cc240b318a9db391a81919c88f964fbf
shipjh <ship_reappearance@naver.com>
2022-04-16 00:36:44 +0900

    test : 랜덤번호 생성 자릿수 테스트 및 역할 분담
    랜덤번호를 생성하는 주체를 컴퓨터에게 역할을 줌.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,14 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import static baseball.util.LengthUtil.getNumberLength;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 컴퓨터_객체_랜덤번호_사이값_테스트() {
+        int computerNumber = Randoms.pickNumberInRange(0, 999);
+        assertThat(computerNumber).isBetween(0, 999);
+    }
+
+    @Test
+    void 컴퓨터_객체_랜덤번호_자릿수_테스트() {
+        int computerNumberLength = getNumberLength(Randoms.pickNumberInRange(0, 999));
+        assertEquals(3, computerNumberLength);
+    }
+
+
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
